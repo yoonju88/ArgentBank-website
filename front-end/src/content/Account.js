61f025e7084dc8af154990f1eadc7b5887cc89f7
yoonju88 <yoonju.t@gmail.com>
2024-07-23 14:13:07 +0200

    Added account component

---
+++ front-end/src/content/Account.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Button from '../components/Button'
+
+function AccountItem ({title, amount, description}) {
+
+    return (
+        <>
+            <section className="account">
+                <div className="account-content-wrapper">
+                    <h3 className="account-title">{title}</h3>
+                    <p className="account-amount">{amount}</p>
+                    <p className="account-amount-description">{description}</p>
+                </div>
+                <div className="account-content-wrapper cta">
+                    <Button 
+                    className="transaction-button"
+                    >
+                        View transactions
+                    </Button>
+                </div>
+            </section>
+        </>
+    )
+}
+export default AccountItem
--- front-end/src/pages/profile.js
+++ front-end/src/pages/profile.js
@@ -1,5 +1,7 @@
 import { useSelector } from "react-redux";
-
+import AccountItem from "../content/Account";
+import { accountList } from "../content/data/accountList"
+import Button from "../components/Button";
 function Profile() {
    const {user} = useSelector((state)=> state.auth);
    
@@ -7,39 +9,17 @@ function Profile() {
         <>
             <div className="header">
                 <h1>Welcome back <br /> {user?.firstName} {user?.lastName}!</h1>
-                <button className="edit-button">Edit Name</button>
+                <Button className="edit-button">Edit Name</Button>
             </div>
             <h2 className="sr-only">Accounts</h2>
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-                    <p className="account-amount">$2,082.79</p>
-                    <p className="account-amount-description">Available Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className="transaction-button">View transactions</button>
-                </div>
-            </section>
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-                    <p className="account-amount">$10,928.42</p>
-                    <p className="account-amount-description">Available Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className="transaction-button">View transactions</button>
-                </div>
-            </section>
-            <section className="account">
-                <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-                    <p className="account-amount">$184.30</p>
-                    <p className="account-amount-description">Current Balance</p>
-                </div>
-                <div className="account-content-wrapper cta">
-                    <button className="transaction-button">View transactions</button>
-                </div>
-            </section>
+            {accountList.map (e => (
+                <AccountItem 
+                    key={e.id}
+                    title={e.title}
+                    amount={e.amount}
+                    description={e.description}
+                />
+            ))}
         </>
     )
 }
