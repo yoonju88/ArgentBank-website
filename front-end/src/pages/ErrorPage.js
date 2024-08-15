68cd311b920115884ee88fd749b7037c88cb3ecb
yoonju88 <yoonju.t@gmail.com>
2024-08-15 13:31:13 +0200

    corrected spelling on the text

--- front-end/src/pages/ErrorPage.js
+++ front-end/src/pages/ErrorPage.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 function ErrorPage (){
     return (
     <div className='error-container'>
-            <p className='error-page-msg'>The page you request <br/> dose not exist...</p>
+            <p className='error-page-msg'>The page you requested <br/> doesn't exist...</p>
             <p className='error-link'> 
                 <Link to='/'>
                 Return to home page
